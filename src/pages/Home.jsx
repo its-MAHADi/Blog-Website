772166a11fa1063e8162852d6bac8bd66405ed86
its-MAHADi <mhkhandakar57@gmail.com>
2025-06-16 22:05:52 +0600

    Recent blog section done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Slider from './Shared/Slider'
 import { useLoaderData } from 'react-router'
+import RecentBlog from './Shared/RecentBlog'
 
 const Home = () => {
     const blogData = useLoaderData()
   return (
     <div>
      <Slider blogs={blogData}></Slider>
-     
+     <RecentBlog key={blogData._id} blogCards={blogData}></RecentBlog>
     </div>
   )
 }
